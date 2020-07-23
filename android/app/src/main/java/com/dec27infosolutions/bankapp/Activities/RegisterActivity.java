cc18056cf1b9f558cbf32263f735e1cbef6b3cd8
surajkamblesk <alphacoderofficial@gmail.com>
2020-07-23 14:16:57 +0530

    added login register and creditcard activities

---
+++ android/app/src/main/java/com/dec27infosolutions/bankapp/Activities/RegisterActivity.java
@@ -0,0 +1,31 @@
+package com.dec27infosolutions.bankapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.dec27infosolutions.bankapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    AutoCompleteTextView username, emailreg , pass, confirm_pass ;
+    Button btnSignup ;
+    TextView tvSignin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+        username = findViewById(R.id.atvUsernameReg);
+        emailreg = findViewById(R.id.atvEmailLog);
+        pass = findViewById(R.id.atvPasswordLog);
+        confirm_pass = findViewById(R.id.atvPasswordConfirm);
+
+        
+    }
+}
